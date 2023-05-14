78a8d5b39e34704286c9c6f2bafad8b3bf1f699e
Yavuz Yılmaz <yavuzyilmaz@eskisehir.edu.tr>
2023-05-14 13:34:19 +0300

    Last version is updated

---
+++ src/main/java/WebServerDemo/ECommerceDemo/business/concretes/PaymentManager.java
@@ -0,0 +1,6 @@
+package WebServerDemo.ECommerceDemo.business.concretes;
+
+import WebServerDemo.ECommerceDemo.business.abstracts.PaymentService;
+
+public class PaymentManager implements PaymentService {
+}
--- src/main/java/WebServerDemo/ECommerceDemo/business/concretes/ProductManager.java
+++ src/main/java/WebServerDemo/ECommerceDemo/business/concretes/ProductManager.java
@@ -1,38 +1,56 @@
 package WebServerDemo.ECommerceDemo.business.concretes;
 
 import WebServerDemo.ECommerceDemo.business.abstracts.ProductService;
+import WebServerDemo.ECommerceDemo.business.requests.CreateProductRequest;
+import WebServerDemo.ECommerceDemo.business.requests.UpdateProductRequest;
+import WebServerDemo.ECommerceDemo.business.responses.GetAllProductResponse;
+import WebServerDemo.ECommerceDemo.business.responses.GetByIdProductResponse;
+import WebServerDemo.ECommerceDemo.core.utilities.mappers.ModelMapperService;
 import WebServerDemo.ECommerceDemo.dataAccess.ProductRepository;
 import WebServerDemo.ECommerceDemo.entities.Product;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 @AllArgsConstructor
 public class ProductManager implements ProductService {
     private ProductRepository productRepository;
+    private ModelMapperService modelMapperService;
 
     @Override
-    public List<Product> getAll() {
-        return productRepository.findAll();
+    public List<GetAllProductResponse> getAll() {
+        List<Product> products = productRepository.findAll();
+        List<GetAllProductResponse> productResponse = products.stream()
+                .map(product -> this.modelMapperService.forResponse().map(product, GetAllProductResponse.class))
+                .collect(Collectors.toList());
+
+        return productResponse;
+
     }
 
     @Override
-    public Optional<Product> getById(int id) {
-       return productRepository.findById(id);
+    public GetByIdProductResponse getById(int id) {
+        Product product = this.productRepository.findById(id).orElseThrow();
+
+        GetByIdProductResponse response = this.modelMapperService.forResponse().map(product, GetByIdProductResponse.class);
+
+        return response;
     }
 
 
     @Override
-    public void add(Product product) {
-        productRepository.save(product);
+    public void add(CreateProductRequest createProductRequest) {
+        Product product = this.modelMapperService.forRequest().map(createProductRequest, Product.class);
+        this.productRepository.save(product);
     }
 
     @Override
-    public void update(Product product) {
-        productRepository.save(product);
+    public void update(UpdateProductRequest updateProductRequest) {
+        Product product = this.modelMapperService.forRequest().map(updateProductRequest, Product.class);
+        this.productRepository.save(product);
     }
 
     @Override
