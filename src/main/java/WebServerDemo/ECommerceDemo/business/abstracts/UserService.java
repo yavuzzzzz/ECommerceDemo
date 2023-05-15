321e29ef70176a6c2daad41dc8ebead57ed26789
malierel <malierel16@gmail.com>
2023-05-15 20:56:53 +0300

    Added UserService.java

--- src/main/java/WebServerDemo/ECommerceDemo/business/abstracts/UserService.java
+++ src/main/java/WebServerDemo/ECommerceDemo/business/abstracts/UserService.java
@@ -1,4 +1,28 @@
 package WebServerDemo.ECommerceDemo.business.abstracts;
 
-public class UserService {
+import WebServerDemo.ECommerceDemo.entities.Product;
+
+import graphql.schema.DataFetcher;
+import WebServerDemo.ECommerceDemo.entities.User;
+
+import java.util.List;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+public interface UserService {
+
+    User getUserById(Long userId);
+
+    User getUserInfo(String email);
+
+    Page<User> getAllUsers(Pageable pageable);
+
+    List<Product> getCart(List<Long> perfumeIds);
+
+    User updateUserInfo(String email, User user);
+
+    DataFetcher<List<User>> getAllUsersByQuery();
+
+    DataFetcher<User> getUserByQuery();
 }
